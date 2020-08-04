47441f8665904cea2b9882bab0a4c7aa4ab3caef
Enrique Cruz <enrique.cs095@gmail.com>
2020-08-04 02:18:10 -0600

    Se añadio el backend de extraer menu y reviews

---
+++ Backend-UX/Controllers/ReviewsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Backend_UX.Models;
+
+namespace Backend_UX.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReviewsController : Controller
+    {
+
+        public Models.ProyectoUXContext db;
+        public ReviewsController(Models.ProyectoUXContext reviews)
+        {
+            db = reviews;
+        }
+
+        [HttpPost("postReviews{id}")]
+        public IActionResult postReviews(int id)
+        {
+            var getdata = (from r in db.Reviews.Where(options => options.ProductosIdProducto == id )
+                           select new { r.Mensaje, r.Fecha, r.Valoracion, r.ProductosIdProducto}).ToList();
+            return Json(getdata);
+        }
+
+        [HttpGet("getReviews{id}")]
+        public IActionResult getReviews(int id)
+        {
+            var getdata = (from r in db.Reviews.Where(options => options.ProductosIdProducto == id)
+                           select new { r.Mensaje, r.Fecha, r.Valoracion, r.ProductosIdProducto }).ToList();
+            return Json(getdata);
+        }
+
+    }
+}
