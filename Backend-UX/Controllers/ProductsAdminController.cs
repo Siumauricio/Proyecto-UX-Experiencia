883157ecffe54a7579f2cd3c7a8edafbbf83b1f2
Siumauricio <siumauricio@unitec.edu>
2020-08-25 03:51:07 -0600

    Compras, ordenes , detalles terminado

--- Backend-UX/Controllers/ProductsAdminController.cs
+++ Backend-UX/Controllers/ProductsAdminController.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Backend_UX.Models;
+using Proyecto_UX_1.Models;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace Backend_UX.Controllers
-{
+namespace Proyecto_UX_1.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class ProductsAdminController : Controller
-    {
+    public class ProductsAdminController:Controller {
         public Models.ProyectoUXContext db;
         public ProductsAdminController(Models.ProyectoUXContext products) {
             db = products;
@@ -22,34 +20,31 @@ namespace Backend_UX.Controllers
         [HttpGet("getProductsByMenu{id}")]
         public IActionResult getProductsByMenu(int id) {
             var data = (from p in db.Productos.Where(option => option.MenuIdMenu == id)
-                        select new { p.IdProducto, p.Precio,p.Nombre, p.MenuIdMenu, p.Descripcion, p.Url }).ToList();
-                return Json(data);
+                        select new { p.IdProducto, p.Precio, p.Nombre, p.MenuIdMenu, p.Descripcion, p.Url }).ToList();
+            return Json(data);
         }
 
         [HttpGet("getProductById{id}")]
         public IActionResult getProductById(int id) {
             var data = (from p in db.Productos.Where(option => option.IdProducto == id)
                         select new { p.IdProducto, p.Precio, p.Nombre, p.MenuIdMenu, p.Descripcion, p.Url }).ToList();
-            if (data.Count()==0)
-            {
-              return Ok(false);
-               /// return BadRequest(new { message = "Producto no existe" });
+            if (data.Count() == 0) {
+                return Ok(false);
+                /// return BadRequest(new { message = "Producto no existe" });
             }
             return Json(data);
         }
 
 
         [HttpPost("addProduct")]
-        public IActionResult addProduct([FromBody] Productos productos) 
-        {
+        public IActionResult addProduct([FromBody] Productos productos) {
             db.Productos.Add(productos);
             db.SaveChanges();
             return Ok();
         }
 
         [HttpDelete("deleteProduct{id}")]
-        public IActionResult deleteProduct(int id)
-        {
+        public IActionResult deleteProduct(int id) {
             var product = new Productos { IdProducto = id };
             db.Productos.Remove(product);
             db.SaveChanges();
@@ -57,8 +52,7 @@ namespace Backend_UX.Controllers
         }
 
         [HttpPut("putProduct")]
-        public IActionResult putProduct([FromBody] Productos producto)
-        {
+        public IActionResult putProduct([FromBody] Productos producto) {
             db.Productos.Attach(producto);
             db.Entry(producto).State = EntityState.Modified;
             db.SaveChanges();
